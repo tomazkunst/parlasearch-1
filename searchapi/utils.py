8e7180930f800a57dc4c7c4c8e7350795aaac096
muki <filipdobranic@gmail.com>
2016-08-11 00:10:57 +0200

    added highlighting enrichment

--- searchapi/utils.py
+++ searchapi/utils.py
@@ -25,3 +25,23 @@ def enrichQuery(data):
     enrichedData = data
 
     return enrichedData
+
+def enrichHighlights(data):
+
+    results = []
+
+    for i, highlight in enumerate(data['highlighting']):
+
+        hkey = data['highlighting'].keys()[i]
+        speechdata = requests.get('https://data.parlameter.si/v1/getSpeechData/' + hkey.split('g')[1]).json()
+
+        try:
+            results.append({'person': requests.get('https://analize.parlameter.si/v1/utils/getPersonData/' + str(speechdata['speaker_id'])).json(), 'content_t': data['highlighting'][hkey]['content_t'], 'date': speechdata['date']})
+        except ValueError:
+            results.append({'person': {'party': {'acronym': 'unknown', 'id': 'unknown', 'name': 'unknown'}, 'name': 'unknown', 'gov_id': 'unknown', 'id': speechdata['speaker_id']}, 'content_t': data['highlighting'][hkey]['content_t'], 'date': speechdata['date']})
+
+    data['highlighting'] = results
+
+    enrichedData = data
+
+    return enrichedData
