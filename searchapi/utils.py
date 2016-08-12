83c4df0c226a12772341c0a8dbba1950210bd2a2
muki <filipdobranic@gmail.com>
2016-08-12 14:33:23 +0200

    added speech_id to highlighting

--- searchapi/utils.py
+++ searchapi/utils.py
@@ -36,9 +36,9 @@ def enrichHighlights(data):
         speechdata = requests.get('https://data.parlameter.si/v1/getSpeechData/' + hkey.split('g')[1]).json()
 
         try:
-            results.append({'person': requests.get('https://analize.parlameter.si/v1/utils/getPersonData/' + str(speechdata['speaker_id'])).json(), 'content_t': data['highlighting'][hkey]['content_t'], 'date': speechdata['date']})
+            results.append({'person': requests.get('https://analize.parlameter.si/v1/utils/getPersonData/' + str(speechdata['speaker_id'])).json(), 'content_t': data['highlighting'][hkey]['content_t'], 'date': speechdata['date'], 'speech_id': hkey.split('g')[1]})
         except ValueError:
-            results.append({'person': {'party': {'acronym': 'unknown', 'id': 'unknown', 'name': 'unknown'}, 'name': 'unknown', 'gov_id': 'unknown', 'id': speechdata['speaker_id']}, 'content_t': data['highlighting'][hkey]['content_t'], 'date': speechdata['date']})
+            results.append({'person': {'party': {'acronym': 'unknown', 'id': 'unknown', 'name': 'unknown'}, 'name': 'unknown', 'gov_id': 'unknown', 'id': speechdata['speaker_id']}, 'content_t': data['highlighting'][hkey]['content_t'], 'date': speechdata['date'], 'speech_id': hkey.split('g')[1]})
 
     data['highlighting'] = results
 
