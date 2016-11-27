83701b58959542d661493cecee0a1f2e430b403d
Filip Muki Dobranic <filipdobranic@gmail.com>
2016-11-27 18:24:34 +0100

    updated url regex

--- parlasearch/urls.py
+++ parlasearch/urls.py
@@ -23,9 +23,9 @@ urlpatterns = [
 
     url(ur'^mlt/(?P<speech_i>[0-9]+)', mltQuery),
 
-    url(ur'^filter/(?P<words>[čćšžČĆŠŽa-zA-Z0-9 \-\+!"%]+)', filterQuery),
+    url(ur'^filter/(?P<words>[ÖÜØÄÂÁÉÓÚÍÎöüøäâáéóúíîčćšžČĆŠŽa-zA-Z0-9 \-\+!"%]+)', filterQuery),
 
-    url(ur'^q/(?P<words>[čćšžČĆŠŽa-zA-Z0-9 \-\+!"%]+)', regularQuery),
+    url(ur'^q/(?P<words>[ÖÜØÄÂÁÉÓÚÍÎöüøäâáéóúíîčćšžČĆŠŽa-zA-Z0-9 \-\+!"%]+)', regularQuery),
 
     url(ur'^tfidf/s/(?P<session_i>[0-9]+)', tfidfSessionQuery),
 
